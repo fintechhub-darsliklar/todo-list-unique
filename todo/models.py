61f85677e65374a1e94934b245dccc245ff25d84
Ziyodullo <ziyodullodev@gmail.com>
2026-02-12 17:29:19 +0500

    add: news

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class TodoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=50, choices=(
+        ("todo", "Bajarish kerak"),
+        ("complated", "Tugallangan"),
+        ("archive", "Arxivda, Kelajakda bajaraman."),
+    ))
+    date = models.DateField(blank=True, null=True)
+    time = models.TimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.task} | {self.user}"
+    
