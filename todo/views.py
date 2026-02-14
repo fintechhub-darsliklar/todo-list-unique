f569739fa92e3f96a56dd8a3930c64031edf1610
Ziyodullo <ziyodullodev@gmail.com>
2026-02-14 17:04:57 +0500

    Add user authentication features with login and registration pages
    - Implemented home page that redirects unauthenticated users to login
    - Created login page with authentication logic and error handling
    - Developed registration page for new users with validation
    - Added logout functionality to end user sessions
    - Designed HTML templates for index, login, and registration using Tailwind CSS
    - Updated URL routing for user authentication views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import TodoList
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+
+def home_page(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+    data = {
+        "user": request.user,
+    }
+    return render(request, 'index.html', context=data)
+
+
+def login_page(request):
+    data = {}
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("home")
+        data['error'] = "username yoki parol xato"
+    return render(request, 'login.html', context=data)
+
+
+def register_page(request):
+    data = {}
+    if request.method == "POST":
+        ism = request.POST.get("first_name")
+        familya = request.POST.get("last_name")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.filter(username=username)
+        if user:
+            data['error'] = "bu foydalanuvchi mavjud!"
+        else:
+            user = User.objects.create(
+                first_name=ism,
+                last_name=familya,
+                username=username
+            )
+            user.set_password(password)
+            user.save()
+            login(request, user)
+            return redirect('home')
+    return render(request, 'register.html', context=data)
+
+
+def logout_page(request):
+    logout(request)
+    return redirect("login")
